ba9e290713554b37570bb24caae11743905ee2a3
Sean-Jamieson <71718653+Sean-Jamieson@users.noreply.github.com>
2022-12-06 00:05:14 -0700

    lab completed, ready to host

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {origins: 'localhost:8080',}
+});
+
+const port = 8080;
+let users = [];
+io.on('connection', socket => {
+  console.log(`a user connected on ${socket.id}`);
+  socket.on('join', username => {
+    let randNum = Math.floor(Math.random() * (70)) + 1;
+    users.push({username:username, id:randNum}); 
+    socket.username = username;
+    io.emit('user joined', username)
+    io.emit('user list', users);
+  });
+  socket.on('new chat msg', msg => {
+    io.emit('chat msg for all', {msg:msg, username:socket.username});
+  });
+  socket.on('user left', () =>{
+    for(let i = 0; i < users.length; i++){
+      if(users[i].username == socket.username){
+        users.splice(i, 1);
+      }
+    }
+    io.emit('user list', users);
+    io.emit('user left', socket.username);
+  });
+});
+
+server.listen(port, () =>{
+  console.log("server running on port: " + port);
+});
